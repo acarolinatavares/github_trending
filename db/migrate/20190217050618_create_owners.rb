f31542d7bb91f8c968baa76b9ac70fb337450a8b
Ana Carolina Tavares <acarolinatavaress@gmail.com>
2019-02-17 05:21:25 -0300

    Criando owner

--- db/migrate/20190217050618_create_owners.rb
+++ db/migrate/20190217050618_create_owners.rb
@@ -1,39 +1,14 @@
-class CreateClients < ActiveRecord::Migration[5.0]
+class CreateOwners < ActiveRecord::Migration[5.0]
   def change
-    create_table :clients do |t|
-      ### PJ ###
-      t.string :cnpj, limit: 18 #[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}\-?[0-9]{2}
-      t.string :razao_social
-      t.string :inscricao_estadual
-      t.date :data_fundacao
-
-      ### PF ###
-      t.string :nome
-      t.string :rg
-      t.string :orgao_expeditor
-      t.string :estado_civil
-      t.string :profissao
-      t.date :data_nascimento
-
-      ### AMBOS ###
-      t.string :nome_fantasia
-      t.string :ramo_atividade
-      t.string :inscricao_municipal
-      t.string :telefone
-      t.string :cpf
-      t.string :cep
-      t.string :estado
-      t.string :cidade
-      t.string :logradouro
-      t.integer :numero
-      t.string :complemento
-      t.string :bairro
-      t.string :email
-      t.text :observacoes
-
-      ### Informações de máquina ###
-      t.text :primeiro_acesso
-      t.text :segundo_acesso
+    create_table :owners do |t|
+      t.integer :external_id
+      t.string :login
+      t.string :node_id
+      t.string :avatar_url
+      t.string :gravatar_id
+      t.string :url
+      t.string :received_events_url
+      t.string :type
 
       t.timestamps
     end
